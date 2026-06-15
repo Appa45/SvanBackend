7cbbf3163de10eb626240e7713fe88761309e4d9
Ayyappa <s.v.ayyappanaik@gmail.com>
2026-06-15 17:14:12 +0530

    change in the app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,16 @@ const { notFoundHandler, errorHandler } = require("./middlewares/error.middlewar
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://svan-wheat.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
