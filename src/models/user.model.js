3c5fdc894ac33f2d2811e9a09ddb58eb678c76df
Ayyappa <s.v.ayyappanaik@gmail.com>
2026-06-17 21:32:06 +0530

    change in the usermadol

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema(
       trim: true,
       maxlength: 80,
     },
+
     email: {
       type: String,
       required: true,
@@ -16,16 +17,50 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
+
     password: {
       type: String,
       required: true,
       minlength: 8,
       select: false,
     },
+
     role: {
       type: String,
       enum: ["admin", "customer"],
       default: "customer",
+    },
+
+    mobile: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    address: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    dob: {
+      type: String,
+      default: "",
+    },
+
+    wallet: {
+      type: Number,
+      default: 0,
+    },
+
+    referCode: {
+      type: String,
+      default: "",
+    },
+
+    profileImage: {
+      type: String,
+      default: "",
     },
   },
   {
@@ -39,23 +74,24 @@ userSchema.pre("save", async function (next) {
   }
 
   try {
-    this.password = await bcrypt.hash(this.password, 12);
+    this.password = await bcrypt.hash(
+      this.password,
+      12
+    );
     next();
   } catch (error) {
     next(error);
   }
 });
 
-userSchema.methods.comparePassword = async function (
-  candidatePassword
-) {
-  return bcrypt.compare(
-    candidatePassword,
-    this.password
-  );
-};
+userSchema.methods.comparePassword =
+  async function (candidatePassword) {
+    return bcrypt.compare(
+      candidatePassword,
+      this.password
+    );
+  };
 
-// Prevent model overwrite issues
 const User =
   mongoose.models.User ||
   mongoose.model("User", userSchema);
